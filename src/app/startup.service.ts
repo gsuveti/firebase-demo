f5f6c0199b08c41783275997c2496211c6bae78c
georgesuveti <George@1r14n>
2018-05-11 21:07:39 +0300

    firebase: before_demo

---
+++ src/app/startup.service.ts
@@ -0,0 +1,16 @@
+import {Injectable} from '@angular/core';
+import {AngularFirestore} from 'angularfire2/firestore';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class StartupService {
+
+  constructor(private firestore: AngularFirestore) {
+    firestore.firestore.settings({timestampsInSnapshots: true});
+  }
+
+  load() {
+
+  }
+}
