c4c4abb53d689d7d631ac48bb7afb1642d52e650
georgesuveti <George@1r14n>
2018-05-12 01:00:00 +0300

    firebase fix

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,5 @@
 import {BrowserModule} from '@angular/platform-browser';
-import {NgModule} from '@angular/core';
+import {APP_INITIALIZER, NgModule} from '@angular/core';
 
 import {AppRoutingModule} from './app-routing.module';
 import {AppComponent} from './app.component';
@@ -16,6 +16,11 @@ import {
 } from '@angular/material';
 import {NgUploaderModule} from 'ngx-uploader';
 import {FormsModule} from '@angular/forms';
+import {StartupService} from './startup.service';
+
+export function startupServiceFactory(startupService: StartupService): Function {
+  return () => startupService.load();
+}
 
 @NgModule({
   declarations: [
@@ -36,7 +41,14 @@ import {FormsModule} from '@angular/forms';
     MatToolbarModule,
     FormsModule
   ],
-  providers: [],
+  providers: [
+    StartupService,
+    {
+      provide: APP_INITIALIZER,
+      useFactory: startupServiceFactory,
+      deps: [StartupService],
+      multi: true
+    }],
   bootstrap: [AppComponent]
 })
 export class AppModule {
